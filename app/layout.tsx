4e678f34eca753fcce37229a7f3aee0a8cb2d2d6
Simon Tran <thanhnhantran1501@gmail.com>
2023-05-08 15:44:15 -0700

    generate variables and mixins scss files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import "./globals.css";
+import "./globals.scss";
 import { Roboto } from "next/font/google";
 
+//SET UP ROBOTO GOOGLE FONT
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   style: ["italic", "normal"],
@@ -8,6 +9,7 @@ const roboto = Roboto({
   display: "swap",
 });
 
+//SET UP METADATA
 export const metadata = {
   title: "Reapply Dashboard",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>hello</h1>
+      <h2>hello</h2>
     </main>
   );
 }
