5250ea35a0cc70b88a8b86ed6fd43517a1d34caa
Simon Tran <thanhnhantran1501@gmail.com>
2023-05-09 14:59:25 -0700

    create variables for fonts

--- app/components/NavigationComponent/NavigationComponent.tsx
+++ app/components/NavigationComponent/NavigationComponent.tsx
@@ -5,11 +5,26 @@ import NavigationBar from "../NavigationBar/NavigationBar";
 export default function NavigationComponent() {
   return (
     <div className={styles.navigationComponent}>
+      {/* LOGO COMPONENT */}
       <div className={styles.navigationComponentLogoWrapper}>
         <LogoComponent />
         <p className={styles.navigationComponentLogoText}>Realply</p>
       </div>
+      {/* NAVIGATION BAR COMPONENT */}
       <NavigationBar />
+      {/* FREE TRIAL INFORMATION */}
+      <div className={styles.navigationComponentTrialWrapper}>
+        <p className={styles.navigationComponentTrialText}>
+          Your free trial is about to end in <br /> <span> 10 days</span>
+        </p>
+        <p className={styles.navigationComponentTrialSmallText}>
+          You will not be billed during your free trial.
+        </p>
+        <p className={styles.navigationComponentTrialSmallText}>
+          To keep your projects running after the trial end, upgrade to a paid
+          option.
+        </p>
+      </div>
     </div>
   );
 }
