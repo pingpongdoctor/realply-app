226da5070391a7505be928e079d5cb67e4033b1a
Simon Tran <thanhnhantran1501@gmail.com>
2023-05-10 17:04:09 -0700

    finsih building dashboard component

--- app/components/DashboardComponent/DashboardComponent.tsx
+++ app/components/DashboardComponent/DashboardComponent.tsx
@@ -7,9 +7,16 @@ import styles from "./DashboardComponent.module.scss";
 export default function DashboardComponent() {
   return (
     <div className={styles.dashboardComponent}>
+      {/* HEADING TEXT */}
       <h1 className={styles.dashboardComponentHeading}>Dashboard</h1>
+
+      {/* DASHBOARD INFORMATION LIST */}
       <DashboardInforList />
+
+      {/* BAR CHART */}
       <BarChartComponent />
+
+      {/* BAR CHART LABELS */}
       <BarChartLabels />
     </div>
   );
